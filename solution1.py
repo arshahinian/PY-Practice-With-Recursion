0483956183b48afd6f3bf7341e121d11d77f071a
Araz Shahinian <arshahinian@gmail.com>
2022-05-31 20:58:15 -0400

    it was fun

--- solution1.py
+++ solution1.py
@@ -1,1 +1,17 @@
 # Write code for algorithm 1 below
+
+def the_final_countdown(n):
+    if isinstance(n, int) and n > 0:
+        print(n)
+        n = n - 1
+        the_final_countdown(n)
+    elif n == 0:
+        print("launch!")
+    else:
+        print(f"The argument has to be a interger above -1!: {n}")
+the_final_countdown('A')
+the_final_countdown(5.5)
+the_final_countdown(0)
+the_final_countdown(-5)
+the_final_countdown(5)
+
--- solution2.py
+++ solution2.py
@@ -1,1 +1,16 @@
 # Write code for algorithm 2 below
+def print_count_up(a,z):
+    if isinstance(a, int) and a > 0 and isinstance(z, int) and z > a:
+        print(a)
+        a = a + 1
+        print_count_up(a,z)
+    elif a == z:
+        print(a)
+        print("done!")
+    else:
+        print(f"The arguments need to be a interger above 0!: {a},{z}")
+print_count_up('A',5)
+print_count_up(5.5,5)
+print_count_up(0,5)
+print_count_up(-5,5)
+print_count_up(1,5)
